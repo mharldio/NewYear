f1e40293e037623079bc1b188e4d20605ded24d0
jhontimothy <jhontimothyjamesdio@thelewiscollege.edu.ph>
2023-12-31 16:18:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import backgroundImage from './image/year.avif';
 
 const appStyle = {
   textAlign: 'center',
@@ -8,25 +7,32 @@ const appStyle = {
   alignItems: 'center',
   justifyContent: 'center',
   height: '100vh',
-  backgroundImage: `url(${backgroundImage})`, // Use backgroundImage imported correctly
-  backgroundSize: 'cover', // Adjust background size as needed
+  backgroundImage: `url(${require("./image/year.avif")})`,
+  backgroundSize: "100% 100%",
+  backgroundAttachment: "fixed",
 };
 
 const headerStyle = {
-  backgroundColor: 'rgba(40, 44, 52, 0.8)', // Use rgba for a semi-transparent background color
+  backgroundColor: 'rgba(40, 44, 52, 0.8)',
   padding: '20px',
   color: 'white',
-  width: '45%',
-  height: '43%',
+  width: '80%',
   borderRadius: '15px',
   marginTop: '20px',
   fontSize: '19px',
 };
 
+// Media queries for responsiveness
+const mediaQueryMobile = {
+  headerStyle: {
+    width: '40%',
+  },
+};
+
 function App() {
   return (
     <div style={appStyle}>
-      <header style={headerStyle}>
+      <header style={{ ...headerStyle, ...mediaQueryMobile.headerStyle }}>
         <h1>Happy New Year!</h1>
         <p style={{ marginBottom: '5%' }}>
           Wishing you a year filled with joy, success, and prosperity. As we step into 2024, I just wanted to send you a quick note to say thank you for being an amazing friend. May this year bring you lots of laughter, moments of joy, and success in all your endeavors. Here's to new beginnings, shared adventures, and the beautiful friendship we have. Wishing you a fantastic year ahead, filled with love and positivity. Happy New Year, my friend! 🌟🥳
