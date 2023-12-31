91c1aaf643d2e5be68e226d6c2e20b0c71493fbf
jhontimothy <jhontimothyjamesdio@thelewiscollege.edu.ph>
2023-12-31 17:39:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -13,28 +13,39 @@ const appStyle = {
 };
 
 const headerStyle = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   backgroundColor: 'rgba(40, 44, 52, 0.8)',
   padding: '20px',
   color: 'white',
   width: '80%',
   borderRadius: '15px',
-  marginTop: '20px',
   fontSize: '19px',
 };
 
 // Media queries for responsiveness
 const mediaQueryMobile = {
   headerStyle: {
-    width: '40%',
+    width: '90%', // Adjusted width for mobile screens
   },
+  fontSize: '16px', // Adjusted font size for mobile screens
+};
+
+const mediaQueryLandscape = {
+  headerStyle: {
+    width: '50%', // Adjusted width for landscape orientation
+  },
+  fontSize: '18px', // Adjusted font size for landscape orientation
 };
 
 function App() {
   return (
     <div style={appStyle}>
-      <header style={{ ...headerStyle, ...mediaQueryMobile.headerStyle }}>
+      <header style={{ ...headerStyle, ...mediaQueryMobile.headerStyle, ...mediaQueryLandscape.headerStyle }}>
         <h1>Happy New Year!</h1>
-        <p style={{ marginBottom: '5%' }}>
+        <p>
           Wishing you a year filled with joy, success, and prosperity. As we step into 2024, I just wanted to send you a quick note to say thank you for being an amazing friend. May this year bring you lots of laughter, moments of joy, and success in all your endeavors. Here's to new beginnings, shared adventures, and the beautiful friendship we have. Wishing you a fantastic year ahead, filled with love and positivity. Happy New Year, my friend! 🌟🥳
         </p>
         <div style={appStyle.mode}>
